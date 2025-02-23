b9d4da86dab2c49cb66525f7f08f16a070f8adce
avtandil <shainidze@gmail.com>
2025-02-23 10:50:56 +0200

    refactor: change import style from ../ to @/

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import os from 'os';
 
 dotenv.config();
-
+// console.log(os.hostname());
 export const config = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
--- src/controllers/bot-command.controller.ts
+++ src/controllers/bot-command.controller.ts
@@ -6,6 +6,6 @@ import { Commands } from '@/utils/enums.util';
 bot.command(Commands.start, (ctx) => {
   console.log('ctx');
   ctx.reply(
-    'Welcome to the OpenAI Bot! Please enter a prompt to generate text.'
+    'Welcome to the TelegramAI Bot! Please enter a prompt to generate text.'
   );
 });
