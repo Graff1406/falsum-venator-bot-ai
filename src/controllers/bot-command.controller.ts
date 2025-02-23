7a00ac1536a29f1906c2b71abda48fbe1d0fa4cc
avtandil <shainidze@gmail.com>
2025-02-23 12:15:47 +0200

    upadate: add to start comant gemini response with translate in to the current lang og user

--- src/controllers/bot-command.controller.ts
+++ src/controllers/bot-command.controller.ts
@@ -1,10 +1,14 @@
 import bot from '@/providers/bot.provider';
 import { Commands } from '@/utils/enums.util';
+import { welcomeMessage } from '@/utils/ai-prompt.util';
+import { generateText } from '@/services/generateText.services';
 
 // bot.api.setMyCommands([{ command: 'start', description: 'Start the bot' }]);
 
-bot.command(Commands.start, (ctx) => {
-  ctx.reply(
-    'Welcome to the TelegramAI Bot! Please enter a prompt to generate text.'
-  );
+bot.command(Commands.start, async (ctx) => {
+  const lang = ctx.from?.language_code;
+  const { data } = await generateText<string>({
+    prompt: `Translate the message into the specified language:${lang}. message: ${welcomeMessage}. Response must be included only the translated message.`,
+  });
+  ctx.reply(data);
 });
---
+++ src/utils/ai-prompt.util.ts
@@ -0,0 +1,20 @@
+export const welcomeMessage = `
+Welcome to Falsum Venator Bot AI!
+
+I am your personal disinformation detector on Telegram. My job is to help you spot manipulation, propaganda, and misinformation in messages.
+
+How I work:
+- Send me a link to any Telegram channel you want to monitor
+- I will analyze all new posts in this channel
+- For each message, you will receive a detailed analysis of:
+- The level of information reliability
+- Detected manipulations
+- Type of disinformation
+- Hidden motives
+- Target audience
+- Emotional impact
+
+Get started right now:
+Simply send me a link to the Telegram channel you are interested in in the format t.me/channel_name
+Remember: the more channels you monitor, the better protected you are from disinformation.
+`;
