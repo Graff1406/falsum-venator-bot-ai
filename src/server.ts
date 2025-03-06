339bbb9e34e4bad199f91f87adf6ab37c7836ebc
avtandil <shainidze@gmail.com>
2025-03-06 15:49:22 +0200

    fix:Configure paths for using aliases

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import createApp from './app';
 import { config } from './config/app.config';
 import { pingServer } from './utils/ping-server.util';
+import * as tsConfigPaths from 'tsconfig-paths';
 
+// Configure paths for using aliases
+const tsConfigPath = './tsconfig.json';
+const baseUrl = './dist'; // Path to the directory where the compiled files are located
+tsConfigPaths.register({
+  baseUrl,
+  paths: require(tsConfigPath).compilerOptions.paths,
+});
 const startServer = () => {
   const app = createApp();
 
